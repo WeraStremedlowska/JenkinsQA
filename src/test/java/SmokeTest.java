fac2fa812c2376d35d5554e45886423b8ef965d8
Wera Stremedlowska <Stremedlowska@gmail.com>
2024-04-06 19:11:30 +0200

    first commit

---
+++ src/test/java/SmokeTest.java
@@ -0,0 +1,15 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import src.runner.BaseTest;
+
+public class SmokeTest extends BaseTest {
+
+    @Test
+    public void testWelcome() {
+        WebElement welcomeText = getDriver().findElement(By.cssSelector(".empty-state-block > h1"));
+
+        Assert.assertEquals(welcomeText.getText(), "Welcome to Jenkins!");
+    }
+}
