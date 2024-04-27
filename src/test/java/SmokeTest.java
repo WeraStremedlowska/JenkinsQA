79e3a772a79123e246999217b4e344d29b119288
Wera Stremedlowska <Stremedlowska@gmail.com>
2024-04-27 22:51:40 +0200

    video 2

--- src/test/java/SmokeTest.java
+++ src/test/java/SmokeTest.java
@@ -8,8 +8,8 @@ public class SmokeTest extends BaseTest {
 
     @Test
     public void testWelcome() {
-        WebElement welcomeText = getDriver().findElement(By.cssSelector(".empty-state-block > h7"));
+        WebElement welcomeText = getDriver().findElement(By.cssSelector(".empty-state-block > h1"));
 
-        Assert.assertEquals(welcomeText.getText(), "Welcome to Jenkins!");
+        Assert.assertEquals(welcomeText.getText(), " to Jenkins!");
     }
 }
